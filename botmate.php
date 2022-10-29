6549684cac0e0c501de6147e0774cfeb8d7a39d3
Syed Muhammad Usman <smusman98@gmail.com>
2022-10-29 20:29:42 +0500

    Added Freemius

--- botmate.php
+++ botmate.php
@@ -15,6 +15,45 @@
 
 use BotMate\Init;
 
+/**
+ * Initialize Freemius
+ * 
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+if ( ! function_exists( 'bot_fs' ) ) {
+    // Create a helper function for easy SDK access.
+    function bot_fs() {
+        global $bot_fs;
+
+        if ( ! isset( $bot_fs ) ) {
+            // Include Freemius SDK.
+            require_once dirname(__FILE__) . '/includes/freemius/start.php';
+
+            $bot_fs = fs_dynamic_init( array(
+                'id'                  => '11339',
+                'slug'                => 'botmate',
+                'type'                => 'plugin',
+                'public_key'          => 'pk_a4f7ee3bec9ac075b51796f9bb4bb',
+                'is_premium'          => false,
+                'has_addons'          => false,
+                'has_paid_plans'      => false,
+                'menu'                => array(
+                    'slug'           => 'botmate',
+                    'account'        => false,
+                ),
+            ) );
+        }
+
+        return $bot_fs;
+    }
+
+    // Init Freemius.
+    bot_fs();
+    // Signal that SDK was initiated.
+    do_action( 'bot_fs_loaded' );
+}
+
 defined( 'ABSPATH' ) || exit;
 define( 'BOTMATE_VERSION', '1.0.0' );
 define( 'BOTMATE_DB_VERSION', '1.0.0' );
